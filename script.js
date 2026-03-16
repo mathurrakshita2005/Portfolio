7ad4bc33ce2c4ddca9588bea767cb94b0bf525c9
RAKSHITA MATHUR <rakshitamathur@RAKSHITAs-MacBook-Air.local>
2026-03-16 21:21:36 +0530

    first portfolio

---
+++ script.js
@@ -0,0 +1,16 @@
+    // smooth scroll
+
+document.querySelectorAll('nav a').forEach(link => {
+
+    link.addEventListener('click', function(e){
+    
+    e.preventDefault()
+    
+    document.querySelector(this.getAttribute('href'))
+    .scrollIntoView({
+    behavior:"smooth"
+    })
+    
+    })
+    
+    })
